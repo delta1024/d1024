47d731bc9118b7594c5097f60da8e46f5ce357e8
Jacob Stannix <jakestannix at gmail.com>
2021-12-31 15:03:51 -0700

    shells integrated

--- services/packages.scm
+++ services/packages.scm
@@ -1,34 +1,39 @@
 (define-module (d1024 services packages)
-#:use-module (gnu packages)
-#:use-module (gnu packages base)
-#:use-module (gnu packages fonts)
-#:use-module (gnu packages fontutils)
-#:use-module (gnu packages admin)
-#:use-module (gnu packages xorg)
-#:use-module (gnu packages dunst)
-#:use-module (gnu packages linux)
-#:use-module (gnu packages freedesktop)
-#:use-module (gnu packages gnome)
-#:use-module (gnu packages package-management)
-#:use-module (gnu packages compton)
-#:use-module (gnu packages video)
-#:use-module (gnu packages terminals)
-#:use-module (gnu packages wm)
-#:use-module (gnu packages web-browsers)
-#:use-module (gnu packages image-viewers)
-#:use-module (gnu packages mpd)
-#:use-module (gnu packages xdisorg)
-#:use-module (gnu packages pulseaudio)
-#:use-module (gnu packages disk)
-#:use-module (gnu packages shells)
-#:use-module (gnu packages aspell)
-#:use-module (gnu packages password-utils)
-#:use-module (gnu packages gnupg)
-#:use-module (gnu packages compression)
-#:use-module (gnu packages vim)
-#:use-module (gnu packages emacs))
+  #:use-module (gnu packages)
+  #:use-module (gnu packages base)
+  #:use-module (gnu packages fonts)
+  #:use-module (gnu packages fontutils)
+  #:use-module (gnu packages admin)
+  #:use-module (gnu packages xorg)
+  #:use-module (gnu packages dunst)
+  #:use-module (gnu packages linux)
+  #:use-module (gnu packages freedesktop)
+  #:use-module (gnu packages gnome)
+  #:use-module (gnu packages package-management)
+  #:use-module (gnu packages compton)
+  #:use-module (gnu packages video)
+  #:use-module (gnu packages terminals)
+  #:use-module (gnu packages wm)
+  #:use-module (gnu packages web-browsers)
+  #:use-module (gnu packages image-viewers)
+  #:use-module (gnu packages mpd)
+  #:use-module (gnu packages xdisorg)
+  #:use-module (gnu packages pulseaudio)
+  #:use-module (gnu packages disk)
+  #:use-module (gnu packages shells)
+  #:use-module (gnu packages aspell)
+  #:use-module (gnu packages password-utils)
+  #:use-module (gnu packages gnupg)
+  #:use-module (gnu packages compression)
+  #:use-module (nongnu packages mozilla)
+  #:use-module (gnu packages vim)
+  #:use-module (gnu packages emacs))
 
-(define-public wrath-packages
+(define large-packages
+  (list
+   firefox))
+
+(define wrath-packages-base
   (list glibc-locales
         font-abattis-cantarell
         font-fira-code
@@ -46,22 +51,26 @@
         fontconfig
         neovim))
 
+(define-public wrath-packages
+  (append
+   wrath-packages-base))
+   ;; large-packages))
 (define-public desktop-packages
- (list dunst
-       brightnessctl
-       perl-file-mimeinfo
-       network-manager
-       ;;flatpak
-       picom
-       qutebrowser
-       mpv
-       youtube-dl
-       alacritty
-       xmodmap
-       mpd
-       pavucontrol
-       xclip
-       polybar
-       xwallpaper
-       sxiv
-       redshift))
+   (list dunst
+	 brightnessctl
+	 perl-file-mimeinfo
+	 network-manager
+	 ;;flatpak
+	 picom
+	 qutebrowser
+	 mpv
+	 youtube-dl
+	 alacritty
+	 xmodmap
+	 mpd
+	 pavucontrol
+	 xclip
+	 polybar
+	 xwallpaper
+	 sxiv
+	 redshift))
--- services/shells.scm
+++ services/shells.scm
@@ -2,27 +2,164 @@
   #:use-module (gnu home)
   #:use-module (gnu home services)
   #:use-module (gnu home services shells)
+  #:use-module (gnu packages vim)
   #:use-module (gnu services)
   #:use-module (guix gexp))
 
-(define my-zprofile
-  (plain-file "zprofile"
-	      "\
-if [[ -d \"$HOME/.bin\" ]]; then
-    export PATH=\"$PATH:$HOME/.bin\"
+(define preamble "\
+[[ `whoami` == root ]] && source /root/.config/zsh/.zshrc
+GPG_TTY=$(tty)
+export GPG_TTY
+# If not running interactively, don't do anything
+[[ $- != *i* ]] && return")
+
+(define tab-complete "\
+# Basic auto/tab complete
+autoload -Uz compinit promptinit
+zstyle ':completion:*' menu select
+zmodload zsh/complist
+compinit
+_comp_options+=(globdots)
+promptinit; 
+prompt walters")
+
+(define vi-mode "\
+#Vi Mode
+bindkey -v
+export 
+
+# Use vim keys in tab complete menu:
+bindkey -M menuselect 'h' vi-backward-char
+bindkey -M menuselect 'k' vi-up-line-or-history
+bindkey -M menuselect 'l' vi-forward-char
+bindkey -M menuselect 'j' vi-down-line-or-history
+bindkey -v '^?' backward-delete-char
+
+# Edit line in vim with ctrl-e
+autoload edit-command-line; zle -N edit-command-line
+bindkey '^e' edit-command-line")
+
+(define extractor "\
+# # ex = Extractor for all kinds of archives
+# # usage: ex <file>
+ex ()
+{
+  if [ -f $1 ] ; then
+    case $1 in
+      ,*.tar.bz2)   tar xjf $1   ;;
+      ,*.tar.gz)    tar xzf $1   ;;
+      ,*.bz2)       bunzip2 $1   ;;
+      ,*.rar)       unrar x $1   ;;
+      ,*.gz)        gunzip $1    ;;
+      ,*.tar)       tar xf $1    ;;
+      ,*.tbz2)      tar xjf $1   ;;
+      ,*.tgz)       tar xzf $1   ;;
+      ,*.zip)       unzip $1     ;;
+      ,*.Z)         uncompress $1;;
+      ,*.7z)        7z x $1      ;;
+      ,*.deb)       ar x $1      ;;
+      ,*.tar.xz)    tar xf $1    ;;
+      ,*)           echo \"'$1' cannot be extracted via ex()\" ;;
+    esac
+  else
+    echo \"'$1' is not a valid file\"
+  fi
+}")
+
+(define lfcd "\
+lfcd () {
+tmp=\"$(mktemp)\"
+lf -last-dir-path=\"$tmp\" \"$@\"
+if [ -f \"$tmp\" ]; then
+        dir=\"$(cat \"$tmp\")\"
+        rm -f \"$tmp\" > /dev/null
+        [ -d \"$dir\" ] && [ \"$dir\" != \"$(pwd)\" ] && cd \"$dir\"
 fi
-# . \"$HOME/.bin/guix-activate-profiles\"
-# . \"$HOME/.bin/guix-activate-xdg-data\"
-export $(dbus-launch)"))
+}
+bindkey -s '^o' lfcd")
+
+
+(define ls-alias
+  '( ( "ls" . "'ls --color=auto -lh'")
+     ( "la" . "'/usr/bin/env -S ls --color=auto -a'")
+     ( "ll" . "'/usr/bin/env -S ls --color=auto -la'")
+     ( "l" . "'/usr/bin/env -S ls --color=auto'")))
+
+(define typos-grep-alias
+  '( ( ".." . "'cd ..'")
+     ( "pdw" . "pwd")
+     ( "grep" . "grep --color=auto")
+     ( "egrep" . "egrep --color=auto")
+     ( "fgrep" . "fgrep --color=auto")))
+
+(define git-alias
+  '( ( "ga" . "\"git add\"")
+     ( "gau" . "\"git add -u\"")
+     ( "gc" . "\"git commit\"")
+     ( "gcm" . "\"git commit -m\"")
+     ( "gC" . "\"git checkout\"")
+     ( "gp" . "\"git push\"")
+     ( "gpu" . "\"git push -u\"")
+     ( "gs" . "\"git status\"")))
+
+(define cargo-alias
+  '( ( "bcr" . "RUST_BACKTRACE=1 cargo run")
+     ( "cr" . "cargo run")
+     ( "ct" . "cargo test")
+     ( "cbr" . "cargo build --release")
+     ( "cch" . "cargo check")
+     ( "ccD" . "cargo doc --document-private-items")
+     ( "cCD" . "cargo doc --document-private-items --open")
+     ( "ccd" . "cargo doc")
+     ( "cCd" . "cargo doc --open")
+     ( "ccf" . "cargo fmt")
+     ( "cvm" . "nvim src/main.rs")
+     ( "cem" . "emacsclient -nw src/main.rs" )
+     ( "rustdoc" . "rustup doc&|")))
+
+(define zalias
+  (append
+   ls-alias
+   typos-grep-alias
+   git-alias
+   cargo-alias))
+
+(define build-alias-string
+  (lambda (alias)
+    (string-append "alias " (car alias) "=" (cdr alias) "\n")))
+
+(define aliasrc
+  "")
+(define append-aliasrc
+  (lambda (item)
+    (set! aliasrc
+	  (string-append aliasrc item))))
+
+(define build-alias-file
+  (let ((strings (map build-alias-string zalias)))
+    (map append-aliasrc strings)))
 
 (define-public shell-services
   (list
-    (service home-zsh-service-type
-	     (home-zsh-configuration
-	      (zprofile `(,my-zprofile))
-	      (zshrc `(,(local-file (string-append (getenv "HOME") "/.system/d1024/services/zsh/zshrc"))))))
+   (service home-zsh-service-type
+	    (home-zsh-configuration
+	     (zprofile `(,(plain-file "zprofile"
+				      "export $(dbus-launch)")))
+	     (environment-variables '(( "PATH" .
+					"$PATH:$HOME/.scripts/:$HOME/.scripts/web:$HOME/.scripts/status-modules:$HOME/.cargo/bin:$HOME/.bin")
+		( "HISTFILE" . "~/.cache/zsh/history")
+		( "SAVEHIST" . "10000")
+		( "HISTSIZE" . "10000")
+		( "KEYTIMEOUT" . "1")))
+	     (zshrc `(,(plain-file "zshrc"
+				   (string-append
+				    preamble "\n"
+				    tab-complete "\n"
+				    vi-mode "\n"
+				    extractor "\n"
+				    lfcd "\n"
+				    "\
+#source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+#source /usr/share/zsh/plugins/zsh-you-should-use/you-should-use.plugin.zsh
+autoload autosuggestions" "\n" aliasrc))))))))
 
-    (simple-service 'zsh-alias
-		    home-files-service-type
-		    (list `("config/zsh/aliasrc"
-			    ,(local-file (string-append (getenv "HOME") "/.system/d1024/services/zsh/aliasrc")))))))
